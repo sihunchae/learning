eb9430f26c7d172c9c47db1f7a1c8acd37030b24
sihunchae <sihunchae59@gmail.com>
2020-01-25 01:56:51 +0900

    improve readability

--- python3/dynamic-programming/1436. make-to-1.py
+++ python3/dynamic-programming/1436. make-to-1.py
@@ -4,11 +4,10 @@ li = [0, 0, 1, 1]
 if num > 4:
     N = 4
     while N != num + 1:
-        if N % 3 == 0:
-            if N % 2 == 0:
-                li.append(min(li[N - 1] + 1, li[N // 2] + 1, li[N // 3] + 1))
-            else:
-                li.append(min(li[N - 1] + 1, li[N // 3] + 1))
+        if N % 6 == 0:
+            li.append(min(li[N - 1] + 1, li[N // 2] + 1, li[N // 3] + 1))
+        elif N % 3 == 0:
+            li.append(min(li[N - 1] + 1, li[N // 3] + 1))
         elif N % 2 == 0:
             li.append(min(li[N - 1] + 1, li[N // 2] + 1))
         else:
